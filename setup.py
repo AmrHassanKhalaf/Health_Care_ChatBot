27a9e077ba55f808f7ad1655eea164d597e61b4b
AmrHassanKhalaf <amrhassankofficial@gmail.com>
2025-08-28 18:47:57 +0300

    commit

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name="Health_Care_ChatBot",
     version="0.1.0",
     author="Amr Hassan",
+    auther="Moustafa Essam",
     author_email="amrhassankofficial@gmail.com",
+    author_email="elde4aismail111@gmail.com",
     packages=find_packages(),
     install_requires=[]
     )
